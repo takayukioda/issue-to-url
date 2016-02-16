54e4a0d51faedd81689ebe242ae0e5be377485b6
takayukioda <takayuki@odashi.me>
2016-02-16 18:36:50 +0900

    Fix url usage; node.js don't have `URL` object

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,15 +1,17 @@
 'use strict';
 
 var
-  githublink = require(`${__dirname}/../lib/githublink`),
+  issueToUrl = require(`${__dirname}/../lib/main.js`),
   url = require('url');
 
-describe("githublink", () => {
+describe("issue-to-link", () => {
   var
     username = 'takayukioda',
     reponame = 'github-link',
     issuenum = 4;
   it("should return with url form when succeed", () => {
     var issue = `${username}/${reponame}#${issuenum}`;
+    var uri = url.parse(issueToUrl(issue));
+    assert.isDefined(uri);
   });
 });
